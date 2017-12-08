5ef2039666d230e434e24ac2fddf9ac1065ad929
gaoshiyao <shiyao@spacesystech.com>
2017-12-08 17:47:49 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <button @click="clock()">点击</button>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+import Vue from 'vue';
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    clock: function() {
+      $.ajax({
+        url:'/api/gsy',
+        success:function(data){
+          console.log(data);
+        }
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
