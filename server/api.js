e8fd6cdcacdca48de9195f087c8166a981f5f0da
gaoshiyao <shiyao@spacesystech.com>
2017-12-12 14:23:28 +0800

    ajax 跨域

--- server/api.js
+++ server/api.js
@@ -2,19 +2,20 @@
 "use strict";
 const models = require('./db');
 const express = require('express');
+const $  = require('jquery');
 const router = express.Router();
-
+const url = require('url');
 /************** 创建(create) 读取(get) 更新(update) 删除(delete) **************/
 
 // 创建账号接口
-router.post('/api/login/createAccount',(req,res) => {
+router.post('/api/login/createAccount', (req, res) => {
     // 这里的req.body能够使用就在index.js中引入了const bodyParser = require('body-parser')
     let newAccount = new models.Login({
-        account : req.body.account,
-        password : req.body.password
+        account: req.body.account,
+        password: req.body.password
     });
     // 保存数据newAccount数据进mongoDB
-    newAccount.save((err,data) => {
+    newAccount.save((err, data) => {
         if (err) {
             res.send(err);
         } else {
@@ -23,9 +24,25 @@ router.post('/api/login/createAccount',(req,res) => {
     });
 });
 // 获取已有账号接口
-router.get('/api/gsy',(req,res) => {
+router.get('/api/gsy', (req, res) => {
     // 通过模型去查找数据库
-   res.send("gsy");
+    res.send("gsy");
 });
 
+router.post('/gsy', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    var data = {'name': 'jifeng', 'company': 'taobao'};
+    var params=url.parse(req.url,true);
+    //console.log('请求1:'+params);
+    if (params.query && params.query.callback) {
+      //console.log('请求2:'+params.query);
+      var str =  params.query.callback + '(' + JSON.stringify(data) + ')';//jsonp
+      res.send(str);
+    } else {
+      res.send(JSON.stringify(data));//普通的json
+    }
+    // // 通过模型去查找数据库
+    // var r = [{"_name":"湖南省","_regionId":134},{"_name":"北京市","_regionId":143}];
+    // res.send("callback("+JSON.stringify(r)+")");
+});
 module.exports = router;
---
+++ src/components/first.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        first:{{msg}}  {{this.$route.params.name}}
+        <button id='btn' @click="kua()">跨域</button>
+    </div>
+</template>
+<script>
+import $ from "jquery";
+export default {
+  data: function() {
+    return {
+      msg: this.$route.params.name
+    };
+  },
+  methods: {
+    kua:function() {
+      $.getJSON("http://localhost:8088/gsy?id=0&callback=?", function(
+        json
+      ) {
+        console.log(json);
+      });
+    }
+  },
+  watch: {},
+  mounted() {
+    console.log("mounted  params :", this.$route.params);
+    // $.ajax({
+    //   url: "http://localhost:8088/gsy",
+    //   dataType: "jsonp",
+    //   jsonpCallback: "cb",
+    //   success: function(data) {
+    //     console.log(data);
+    //   }
+    // });
+    // $.getJSON("http://localhost:8088/gsy?jsoncallback=?", function(json) {});
+  }
+};
+</script>
